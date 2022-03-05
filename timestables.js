abaadcef94d9ddf388d1dc57bbc9a57afc2bbca3
Simon Laing <laingster@gmail.com>
2022-03-05 09:02:24 +0000

    Enable fullscreen toggling

--- timestables.js
+++ timestables.js
@@ -258,6 +258,21 @@ class Title {
         this.results = results;
     }
 
+    toggleFullScreen() {
+        var doc = window.document;
+        var docEl = doc.documentElement;
+
+        var requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
+        var cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
+
+        if(!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
+            requestFullScreen.call(docEl);
+        }
+        else {
+            cancelFullScreen.call(doc);
+        }
+    }
+
     update() {
         var title = "Times tables";
 
@@ -272,6 +287,7 @@ class Title {
         }
 
         $(".title").html(title);
+        $(".title").click(this.toggleFullScreen);
         return;
     }
 }
