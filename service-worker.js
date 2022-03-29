9f477442b070b1068a5179d46e53e70c69028638
Simon Laing <laingsimon@users.noreply.github.com>
2022-03-29 21:31:23 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,14 @@
 //https://blog.betapage.co/how-to-add-add-to-homescreen-popup-in-web-app-99d5237fabff
+https://dev.to/iamsahilsonawane/a2hs-in-flutter-web-3cln
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      // Cache hit - return response
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
